df8bf9ec0d533a0e01de34a47f412e8cf54039c8
JvVinicius <jvviniciusalbuquerque@gmail.com>
2022-02-11 21:09:59 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    responsive:{
+        0:{
+            items:2
+        },
+        500:{
+            items:4
+        },
+        1000:{
+            items:8
+        }
+    }
+})
